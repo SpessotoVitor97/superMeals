85043df2041447059ef0ccf41331abfb7bfbc84b
Vitor Spessoto <spessoto97@icloud.com>
2022-05-05 00:05:54 -0300

    Started UI

---
+++ superMeals/Features/Recepies/DetailsScreen/Controller/DetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.h
+//  superMeals
+//
+//  Created by Vitor Spessoto on 04/05/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ superMeals/Features/Recepies/DetailsScreen/Controller/DetailsViewController.m
@@ -0,0 +1,31 @@
+//
+//  DetailsViewController.m
+//  superMeals
+//
+//  Created by Vitor Spessoto on 04/05/22.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
